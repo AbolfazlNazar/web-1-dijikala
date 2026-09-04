f4ed8bf971108b4f9110042cc2eae402440f0154
Abolfazl Nazar <abal044nazar@gmail.com>
2026-09-04 19:26:55 +0330

    work on header 2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
+const menuItems = document.querySelectorAll(".sub-menu-item-one");
+const menuContents = document.querySelectorAll(".category-content");
 
+let timer;
+
+menuItems.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    clearTimeout(timer);
+
+    const category = item.dataset.category;
+
+    timer = setTimeout(() => {
+      menuContents.forEach((content) => {
+        content.style.display = "none";
+      });
+
+      const selectedContent = document.querySelector(
+        `[data-content="${category}"]`
+      );
+
+      if (selectedContent) {
+        selectedContent.style.display = "block";
+      }
+    }, 300);
+  });
+});
